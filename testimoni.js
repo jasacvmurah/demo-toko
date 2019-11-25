8ad10044b51db128d0f37021428b93869c5125ac
jasacvmurah <58102699+jasacvmurah@users.noreply.github.com>
2019-11-25 14:26:33 +0700

    Create testimoni.js

---
+++ testimoni.js
@@ -0,0 +1,44 @@
+var dataOrder = {
+    "0877-3523....": {
+        "screenshotURL": "https://1.bp.blogspot.com/-JcrXnxbGmrI/XduAqsTBD6I/AAAAAAAAA7Y/rPsaZyJLS6QCRlbj4Ux1XqrGIyYfzj7fwCLcBGAsYHQ/s1600/testi.jpg",
+        "avaURL": "https://1.bp.blogspot.com/-SspZsHqsMVw/XdkDSecPLdI/AAAAAAAAA1I/w2tkTA3D5o4ZiNrYB5KqJJQx5aTZ1xEpwCLcBGAsYHQ/s1600/WhatsApp%2BImage%2B2019-11-08%2Bat%2B20.20.03.jpeg",
+        "time": "2019-11-24",
+        "template": "Hijab Segi Empat Motif Haisa",
+        "templateURL": "https://desain-toko.blogspot.com/2019/11/kerudung-segi-empat-bahan-voal-premium.html",
+    },
+    "0822-17..77..": {
+        "screenshotURL": "https://1.bp.blogspot.com/-JcrXnxbGmrI/XduAqsTBD6I/AAAAAAAAA7Y/rPsaZyJLS6QCRlbj4Ux1XqrGIyYfzj7fwCLcBGAsYHQ/s1600/testi.jpg",
+        "avaURL": "https://1.bp.blogspot.com/-B94DgCP87XE/XdkEhgPgnfI/AAAAAAAAA1U/hjk6PjF2ewMvKm1A-C6ZpZmsSQfTCt6zgCLcBGAsYHQ/s1600/IMG-20191013-WA0002.jpg",
+        "time": "2019-11-24",
+        "template": "HIjab Segi Empat Motif Maron",
+        "templateURL": "https://desain-toko.blogspot.com/2019/11/terlaris-off-25-rp-20.html",
+    },
+    "082...79..70": {
+        "screenshotURL": "https://1.bp.blogspot.com/-JcrXnxbGmrI/XduAqsTBD6I/AAAAAAAAA7Y/rPsaZyJLS6QCRlbj4Ux1XqrGIyYfzj7fwCLcBGAsYHQ/s1600/testi.jpg",
+        "avaURL": "https://1.bp.blogspot.com/-eft2LkDNLA4/XdkFRRv2yhI/AAAAAAAAA1c/HZaJeSDivoIhYcu7OcArbtlYLZ-ByAlNgCLcBGAsYHQ/s1600/WhatsApp%2BImage%2B2019-11-07%2Bat%2B08.13.31.jpeg",
+        "time": "2019-11-24",
+        "template": "Hijab Polos Bahan Voal",
+        "templateURL": "https://desain-toko.blogspot.com/2019/11/hijab-polos-bahan-voal.html",
+    },
+    "0812-98....55": {
+        "screenshotURL": "https://1.bp.blogspot.com/-JcrXnxbGmrI/XduAqsTBD6I/AAAAAAAAA7Y/rPsaZyJLS6QCRlbj4Ux1XqrGIyYfzj7fwCLcBGAsYHQ/s1600/testi.jpg",
+        "avaURL": "https://1.bp.blogspot.com/-Kb-zQb_Az4A/XdkGDOTPHvI/AAAAAAAAA1k/Li9_N-vb_JgTtc46IcVhemEr3cTVSASYACLcBGAsYHQ/s1600/WhatsApp%2BImage%2B2019-11-05%2Bat%2B16.14.31.jpeg",
+        "time": "2019-11-24",
+        "template": "Hijab Voal Ultima Anti Air",
+        "templateURL": "https://desain-toko.blogspot.com/2019/11/hijab-instan-voal-ultima-anti-air.html",
+    },    
+    "0877-24....93": {
+        "screenshotURL": "https://1.bp.blogspot.com/-JcrXnxbGmrI/XduAqsTBD6I/AAAAAAAAA7Y/rPsaZyJLS6QCRlbj4Ux1XqrGIyYfzj7fwCLcBGAsYHQ/s1600/testi.jpg",
+        "avaURL": "https://1.bp.blogspot.com/-KJdPuhqqduE/XdkHGIK-LyI/AAAAAAAAA1w/GEXOCHn5k0wbQ8eKxqh604CaapBRxdBzwCLcBGAsYHQ/s1600/WhatsApp%2BImage%2B2019-11-02%2Bat%2B10.23.53%2B%25281%2529.jpeg",
+        "time": "2019-11-24",
+        "template": "Hijab Segi Empat Motif Maron",
+        "templateURL": "https://desain-toko.blogspot.com/2019/11/terlaris-off-25-rp-20.html",
+    },       
+     "08..75..55..": {
+        "screenshotURL": "https://1.bp.blogspot.com/-JcrXnxbGmrI/XduAqsTBD6I/AAAAAAAAA7Y/rPsaZyJLS6QCRlbj4Ux1XqrGIyYfzj7fwCLcBGAsYHQ/s1600/testi.jpg",
+        "avaURL": "https://1.bp.blogspot.com/-vQuHHDTCyVM/XdkHwPoHmoI/AAAAAAAAA14/VuasNURi-rwVq9W9HJjMqydKQBTUhIeKQCLcBGAsYHQ/s1600/IMG-20191018-WA0023.jpg",
+        "time": "2019-11-24",
+        "template": "Hijab MOTIF DEENAY ROSE",
+        "templateURL": "https://desain-toko.blogspot.com/2019/11/hijab-motif-deenay-rose.html",
+    },       
+};
